a2a93720c1d6032beb2379b2b6239939a8439fb1
eliyas ch <eliyaschozhakkadofficial@gmail.com>
2022-11-21 12:54:05 +0530

    media files added with category details and slug autopopulation

--- digikart/settings.py
+++ digikart/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#media files configuration
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
