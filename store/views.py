4346d5fa15ac9026eba7983113c3a85d5d85ab70
eliyas ch <eliyaschozhakkadofficial@gmail.com>
2022-11-21 22:25:16 +0530

    store page created

--- store/views.py
+++ store/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+
 from .models import Product
 
 
 # Create your views here.
 def home(request):
 
-    products=Product.objects.all().filter(is_available=True)
-    
-    context={
-        'products':products,
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
     }
-    return render(request,"index.html",context)
+    return render(request, "index.html", context)
+
+
+def store(request):
+
+    products = Product.objects.all().filter(is_available=True)
+    product_count=products.count()
+
+    context = {
+        'products': products,
+        'prodcount':product_count,
+    }
+
+    return render(request, "store/store.html", context)
