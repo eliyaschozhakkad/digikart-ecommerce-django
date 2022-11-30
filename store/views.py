476bcce7a887b282ab6466ac4e339582d4daf024
eliyas ch <eliyaschozhakkadofficial@gmail.com>
2022-11-30 07:13:24 +0530

    search completed successfully

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.cache import cache_control
 from .models import Product
@@ -5,7 +6,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
-from django.http import HttpResponse
+from django.db.models import Q
 
 
 
@@ -66,6 +67,14 @@ def product_detail(request, category_slug, product_slug):
     return render(request, 'store/product_detail.html',context)
 
 def search(request):
-
-    return render(request,'store/store.html')
+    if 'keyword' in request.GET:
+        keyword=request.GET['keyword']
+        if keyword:
+            products=Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context={
+        'products':products,
+        'prod count':product_count,
+        }
+    return render(request,'store/store.html',context)
 
