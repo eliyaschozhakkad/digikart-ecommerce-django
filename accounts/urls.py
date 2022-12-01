009f88f9c605a0fcbf75d87e905687e90a652242
eliyas ch <eliyaschozhakkadofficial@gmail.com>
2022-12-01 19:06:48 +0530

    Forgot password link via email verification added

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path("signin/", views.signin, name='signin'),
     path("signout/", views.signout, name='signout'),
     path("activate/<uidb64>/<token>/", views.activate, name='activate'),
+    path("forgotPassword/", views.forgotPassword, name='forgotPassword'),
+    path("resetpassword_validate/<uidb64>/<token>/", views.resetpassword_validate, name='resetpassword_validate'),
+     path("resetPassword/", views.resetPassword, name='resetPassword'),
+
 
 ]
 
