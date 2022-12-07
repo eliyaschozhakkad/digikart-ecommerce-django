1c5c6a8418356e6dab075a3b4fcd1ea8e6c276b9
eliyas ch <eliyaschozhakkadofficial@gmail.com>
2022-12-07 10:53:04 +0530

    Checkout Page ready

--- digikart/urls.py
+++ digikart/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("accounts/",include('accounts.urls')),
     path("adminpanel/",include('adminpanel.urls')),
     path("cart/",include('carts.urls')),
+    path("orders/",include('orders.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
