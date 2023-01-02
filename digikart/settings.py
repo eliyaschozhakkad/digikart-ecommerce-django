db41398785d0fbd8a6c65093fdf1b53e358539c6
eliyas ch <eliyaschozhakkadofficial@gmail.com>
2023-01-02 13:54:51 +0530

    production ready

--- digikart/settings.py
+++ digikart/settings.py
@@ -19,7 +19,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-live_site=False
+live_site=True
 
 
 
